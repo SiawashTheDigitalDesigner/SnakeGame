3b614fd5dc4b60cabd9b046404277c63736bceb0
siawash <163422079+SiawashTheDigitalDesigner@users.noreply.github.com>
2024-04-24 02:55:07 +0330

    Delete Snake_Game directory

--- Snake_Game/fruit/FruitPosition.h
+++
@@ -1,9 +0,0 @@
-#ifndef FRUID_CORDINATE_H_INCLUDED
-#define FRUID_CORDINATE_H_INCLUDED
-//**Define FruitPosition Structure**
-     typedef struct {
-         int x; // x-coordinate
-         int y; // y-coordinate
-     } FruitPosition;
-
-#endif // FRUID_CORDINATE_H_INCLUDED
